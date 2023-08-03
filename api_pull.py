66a529298675a0fc656c4d84c0535ee50d3f1709
JoelCWiseman <128496183+JoelCWiseman@users.noreply.github.com>
2023-08-03 18:20:50 -0400

    updates

--- api_pull.py
+++ api_pull.py
@@ -12,7 +12,7 @@ PARKS_API_URL = "https://developer.nps.gov/api/v1/parks?api_key=API_KEY"
 ACTIVITIES_API_URL = "https://developer.nps.gov/api/v1/activities/parks?api_key=API_KEY"
 AMENITIES_API_URL = "https://developer.nps.gov/api/v1/amenities?api_key=API_KEY"
 
-# Function to fetch data from the NPS API
+## Function to fetch data from the NPS API
 def fetch_data(api_url):
     params = {"api_key": API_KEY}
     response = requests.get(api_url, params=params)
@@ -20,7 +20,7 @@ def fetch_data(api_url):
 
 # Function to create the parks database
 def create_parks_db():
-    connection = sqlite3.connect("parks.db", charset="utf8")
+    connection = sqlite3.connect("parks.db")
     cursor = connection.cursor()
 
     cursor.execute('''
@@ -34,19 +34,20 @@ def create_parks_db():
     ''')
 
     parks_data = fetch_data(PARKS_API_URL)
-    
-    for park in parks_data["data"]:
-        cursor.execute('''
-            INSERT OR IGNORE INTO parks (parkCode, fullName, stateCode, latitude, longitude)
-            VALUES (?, ?, ?, ?, ?)
-        ''', (park["parkCode"], park["fullName"], park["states"], park["latitude"], park["longitude"]))
+
+    if "data" in parks_data:
+        for park in parks_data["data"]:
+            cursor.execute('''
+                INSERT OR IGNORE INTO parks (parkCode, fullName, stateCode, latitude, longitude)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (park["parkCode"], park["fullName"], park["states"], park["latitude"], park["longitude"]))
 
     connection.commit()
     connection.close()
 
 # Function to create the activities database
 def create_activities_db():
-    connection = sqlite3.connect("activities.db", charset="utf8")
+    connection = sqlite3.connect("activities.db")
     cursor = connection.cursor()
 
     cursor.execute('''
@@ -59,22 +60,24 @@ def create_activities_db():
 
     parks_data = fetch_data(PARKS_API_URL)
 
-    for park in parks_data["data"]:
-        park_code = park["parkCode"]
-        activities_data = fetch_data(f"{ACTIVITIES_API_URL}/{park_code}")
-        activities = [activity["name"] for activity in activities_data["data"]]
-        for activity in activities:
-            cursor.execute('''
-                INSERT INTO activities (parkCode, activityName)
-                VALUES (?, ?)
-            ''', (park_code, activity))
+    if "data" in parks_data:
+        for park in parks_data["data"]:
+            park_code = park["parkCode"]
+            activities_data = fetch_data(f"{ACTIVITIES_API_URL}/{park_code}")
+            if "data" in activities_data:
+                activities = [activity["name"] for activity in activities_data["data"]]
+                for activity in activities:
+                    cursor.execute('''
+                        INSERT INTO activities (parkCode, activityName)
+                        VALUES (?, ?)
+                    ''', (park_code, activity))
 
     connection.commit()
     connection.close()
 
 # Function to create the amenities database
 def create_amenities_db():
-    connection = sqlite3.connect("amenities.db", charset="utf8")
+    connection = sqlite3.connect("amenities.db")
     cursor = connection.cursor()
 
     cursor.execute('''
@@ -87,15 +90,17 @@ def create_amenities_db():
 
     parks_data = fetch_data(PARKS_API_URL)
 
-    for park in parks_data["data"]:
-        park_code = park["parkCode"]
-        amenities_data = fetch_data(f"{AMENITIES_API_URL}/{park_code}")
-        amenities = [amenity["name"] for amenity in amenities_data["data"]]
-        for amenity in amenities:
-            cursor.execute('''
-                INSERT INTO amenities (parkCode, amenityName)
-                VALUES (?, ?)
-            ''', (park_code, amenity))
+    if "data" in parks_data:
+        for park in parks_data["data"]:
+            park_code = park["parkCode"]
+            amenities_data = fetch_data(f"{AMENITIES_API_URL}/{park_code}")
+            if "data" in amenities_data:
+                amenities = [amenity["name"] for amenity in amenities_data["data"]]
+                for amenity in amenities:
+                    cursor.execute('''
+                        INSERT INTO amenities (parkCode, amenityName)
+                        VALUES (?, ?)
+                    ''', (park_code, amenity))
 
     connection.commit()
     connection.close()
